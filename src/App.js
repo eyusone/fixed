0c40b4f89b73dffcc01575035f0c196f2cd7a875
emil <e.yusupovone@yandex.ru>
2018-11-25 22:21:06 +0300

    sss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Inner = styled.div`
+  flex-direction: column;
+  flex: 1;
+  margin-left: ${props => (props.open ? '0px' : '-150px')};
+  width: 50px;
+  transition: all 0.5s ease-in-out;
+  background: lightgreen;  
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  flex-direction: column;
+  background: aqua;
+  width: 100px;
+  margin-left: ${props => (props.opened ? '0px' : '100px')};
+  transition: all 0.5s ease-in-out;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 200px;       
+`;
+
+const Sidebar = styled.div`
+  background: aliceblue;
+  height: 100vh;
+  width: 50px;
+  text-align: center;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      opened: false,
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Container>
+          <Sidebar>
+            <button onClick={()=>{this.setState({ open: !this.state.open, opened: !this.state.opened })}}>open</button>
+          </Sidebar>
+          <Inner 
+            open={this.state.open}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            hello city
+          </Inner>
+          <Content opened={this.state.open}>
+            content
+          </Content>
+        </Container>
     );
   }
 }
